4def27c9b3d4c8e38db39eb1d2b97dba699063eb
harshitsharmaaaa <harshitsharma270905@gmail.com>
2026-01-08 18:25:37 +0000

    building voting dapp

---
+++ anchor/programs/voting/src/lib.rs
@@ -0,0 +1,14 @@
+use anchor_lang::prelude::*;
+use anchor_lang::system_program::{transfer, Transfer};
+
+
+
+declare_id!("9LhAwg9BkbRpdSnRmWJTexVxBQoPrybZ3ee5WMH1kncr");
+
+#[program]
+pub mod voting {
+    use super::*;
+
+}
+
+
